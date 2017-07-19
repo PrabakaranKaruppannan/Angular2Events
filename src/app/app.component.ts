d44e956f1a5ac9d1c84b3c5ef969473f28282f66
Prabakaran, Karuppannan (GfK) <Karuppannan.Prabakaran@gfk.com>
2017-07-19 10:56:34 +0800

    Angular2 Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Events';
+  clickedMessage;
+  keyUpMessage;
+  templateReferenceKeyUpMessage;
+  enterKeyUpMessage;
+
+  clickMe() {
+    this.clickedMessage = 'Button was clicked';
+  }
+
+  keyUp(event: any) {
+    this.keyUpMessage = event.target.value;
+  }
+
+  onKeyUp(value) {
+    this.templateReferenceKeyUpMessage = value;
+  }
+
+  onEnterKeyUp(value) {
+    this.enterKeyUpMessage = value;
+  }
 }
